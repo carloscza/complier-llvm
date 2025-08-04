f6ad9a3c85ecd1ad719b6bb4655d6b4931d02e13
carloscza <carrloscarranza@gmail.com>
2025-08-03 23:18:01 -0700

    Lexer done.

---
+++ tests/lexer_test.c
@@ -0,0 +1,19 @@
+// symbol tokens
+; ~ ^ * % ( ) : { } [ ] + ,
+a b c
+| ||
+& &&
+= ==
+! !=
+< << <=
+> >> >=
+- ->
+d3434_43d
+_d3434_43d
+// comment
+3434273
+343
+-3432
+98904783
+let break continue return loop if else
+//comment
