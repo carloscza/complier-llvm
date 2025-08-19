01d1e65dfb2f658f7c736f107b31870c3a26b766
carloscza <carrloscarranza@gmail.com>
2025-08-18 21:42:06 -0700

    variables finished.

---
+++ tests/variables.c
@@ -0,0 +1,95 @@
+{
+   let x  ;
+
+
+   5 + 3 - 8;
+
+
+   {
+       let x [32] ;
+
+
+       7 + 12 - 18;
+   }
+}
+
+
+let y2;
+y2 = 5 + 3 * 4 - 15;
+y2;
+1 + y2;
+2 + y2 - 5 + y2 * 2 + 1;
+
+
+
+
+// __________________________
+let z
+
+
+       // technically this this valid :D
+
+
+    [5];
+z[0] = 5;
+z[0];
+
+
+let y ;
+y = z[0] + 1;
+y;
+
+
+z[1 + 2] = y + 1;
+
+
+z[1 * 2 + 1];
+// _______________________
+
+
+
+
+if(5) {
+   let i ;
+   i = 8;
+
+
+   let count ;
+   count = 1 + 2;
+
+
+   loop {
+       if !count { break; }
+       count = count - 1;
+
+
+       i;
+       i = i + 1;
+   }
+
+
+   let is[10];
+   count = 0;
+   loop {
+       if count == 10 { break; }
+
+
+       is[count] = 10 + count + 1;
+
+
+       count = count + 1;
+   }
+
+
+   count = 0;
+   loop {
+       if count == 10 { break; }
+       is[count];
+       count = count + 1;
+   }   
+}
+
+
+
+
+// should print 20
